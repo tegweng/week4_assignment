e438b24b2f952ec6d68b9be882ea140b122450dd
Tegwen Gabb <fj803263@live.reading.ac.uk>
2017-10-17 14:14:20 +0100

    Commiting original files from Hillary

---
+++ MTMW12_diffusion-master/README.md
@@ -0,0 +1,1 @@
+Skeleton python and latex code for the MTMW12 assignment on diffusion
---
+++ MTMW12_diffusion-master/pythonCode/diffusion.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+# Outer code for setting up the diffusion problem on a uniform
+# grid and calling the function to perform the diffusion and plot.
+
+from __future__ import absolute_import, division, print_function
+
+import matplotlib.pyplot as plt
+
+# read in all the linear advection schemes, initial conditions and other
+# code associated with this application
+execfile("diffusionSchemes.py")
+execfile("diagnostics.py")
+execfile("initialConditions.py")
+
+def main():
+    # Insert the rest of the code for main here
